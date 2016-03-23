363fc842a3d033ebe23ae7964b91cc12678c76aa
joshlinpillay <joshlinpillay12@gmail.com>
2016-03-24 01:03:58 +0200

    Classes created

--- Employee.cpp
+++ Employee.cpp
@@ -1,11 +1,82 @@
 #include "Employee.h"
+#include<iostream>
+#include <string>
+
+
+using namespace std;
 
 
 Employee::Employee()
 {
+	numberofemployees = 1;
 }
 
 
 Employee::~Employee()
 {
+	numberofemployees = 0;
 }
+
+void Employee::name(string)
+{
+	string temp;
+	cout << "name of employee is " << temp << endl;
+
+
+}
+
+void Employee::staffnumber(int)
+
+{
+	int number;
+	cout << "the staff number of the employee is " << number << endl;
+}
+
+void salaryemployee::setsalary(float)
+{
+
+	float figure;
+	cout << "salary is R" << figure << endl;
+}
+
+void hourlyemployee::sethoursworked(int)
+
+{
+	int hrs;
+	cout << "number of hours worked =" << hrs << endl;
+}
+
+
+void hourlyemployee::sethourlyrate(int, float)
+{
+	float pay;
+	float hrs;
+	float x;
+	pay = hrs * x;
+	cout << "the hourly employee earns " << pay << endl;
+}
+
+void commissionemployee::setsalarybase(float)
+{
+	float base;
+	cout << "the fixed salary base is " << base << endl;
+
+}
+void commissionemployee::setrate(float)
+{
+	float comm;
+	float commission;
+	float base;
+	commission = comm* base;
+	cout << "the commsion earened is " << commission << endl;
+
+}
+
+void commissionemployee::setrevenue(float)
+{
+	float revenue;
+	cout << "the revenue amount is" << revenue << endl;
+}
+
+
+
